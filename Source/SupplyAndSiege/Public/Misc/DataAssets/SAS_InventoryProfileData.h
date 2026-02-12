af2e751c397f9a1f2b371349a7799ecdc27f4a37
Alex M <alex.mount@mtcreative.io>
2026-02-11 21:46:06 -0500

    Started working on the inventory. Incomplete.

---
+++ Source/SupplyAndSiege/Public/Misc/DataAssets/SAS_InventoryProfileData.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataAsset.h"
+#include "SAS_InventoryProfileData.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPPLYANDSIEGE_API USAS_InventoryProfileData : public UDataAsset
+{
+	GENERATED_BODY()
+	
+};
