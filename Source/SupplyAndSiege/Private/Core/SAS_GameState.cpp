28423bcff4e97919db92300549a2e5c94849dae2
MTCreativeDev <alex.mount@mtcreative.io>
2026-02-18 17:26:24 -0500

    Created a custom Game state that has one Inventory Manager Component for each team. Basic functionality created, but WIP.
    Moved the splash textures to the "Assets" Folder Under "Textures"

--- Source/SupplyAndSiege/Private/Core/SAS_GameState.cpp
+++ Source/SupplyAndSiege/Private/Core/SAS_GameState.cpp
@@ -2,4 +2,25 @@
 
 
 #include "Core/SAS_GameState.h"
+#include "Core/Components/SAS_InventoryManagerComponent.h"
 
+ASAS_GameState::ASAS_GameState()
+{
+	//WARNING: This only works for the single player game.
+	
+	InventoryManagerComponentTeam1 = CreateDefaultSubobject<USAS_InventoryManagerComponent>(TEXT("InventoryManagerTeam1"));
+	InventoryManagerComponentTeam2 = CreateDefaultSubobject<USAS_InventoryManagerComponent>(TEXT("InventoryManagerTeam2"));
+	
+	InventoryManagerComponentTeam1->AssignedTeam = ESAS_Team::Team1;
+	InventoryManagerComponentTeam2->AssignedTeam = ESAS_Team::Team2;
+}
+
+USAS_InventoryManagerComponent* ASAS_GameState::GetInventoryManagerForTeam(ESAS_Team Team) const
+{
+	switch (Team)
+	{
+	case ESAS_Team::Team1: return InventoryManagerComponentTeam1;
+	case ESAS_Team::Team2: return InventoryManagerComponentTeam2;
+	default: return nullptr;
+	}
+}
--- Source/SupplyAndSiege/Public/Core/SAS_GameState.h
+++ Source/SupplyAndSiege/Public/Core/SAS_GameState.h
@@ -4,14 +4,28 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameStateBase.h"
+#include "Core/SAS_Enumerators.h"
 #include "SAS_GameState.generated.h"
 
-/**
- * 
- */
+class USAS_InventoryManagerComponent;
+
 UCLASS()
 class SUPPLYANDSIEGE_API ASAS_GameState : public AGameStateBase
 {
 	GENERATED_BODY()
 	
+public:
+	ASAS_GameState();
+
+	UFUNCTION(BlueprintCallable, Category = "Inventory")
+	USAS_InventoryManagerComponent* GetInventoryManagerForTeam(ESAS_Team Team) const;
+
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	USAS_InventoryManagerComponent* InventoryManagerComponentTeam1;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Inventory")
+	USAS_InventoryManagerComponent* InventoryManagerComponentTeam2;
+
 };
