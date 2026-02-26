0fa5752e8f082edfaae02cc7964d422bdd09de59
Cody McCarty <codyjmccarty@gmail.com>
2026-02-26 16:22:31 -0600

    opponent AI

---
+++ Source/SupplyAndSiege/Private/Core/AI/SAS_OpponentAIC.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Core/AI/SAS_OpponentAIC.h"
+
+#include "Components/StateTreeAIComponent.h"
+#include "Core/Components/SAS_UnitManagerComponent.h"
+
+ASAS_OpponentAIC::ASAS_OpponentAIC()
+{
+	UnitManagerComp = CreateDefaultSubobject<USAS_UnitManagerComponent>(TEXT("UnitManagerComp"));
+	UnitManagerComp->SetTeam(ESAS_Team::Team2);
+	
+	StateTreeComp = CreateDefaultSubobject<UStateTreeAIComponent>(TEXT("StateTreeComp"));
+}
---
+++ Source/SupplyAndSiege/Public/Core/AI/SAS_OpponentAIC.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AIController.h"
+#include "SAS_OpponentAIC.generated.h"
+
+class USAS_UnitManagerComponent;
+class UStateTreeAIComponent;
+
+UCLASS()
+class SUPPLYANDSIEGE_API ASAS_OpponentAIC : public AAIController
+{
+	GENERATED_BODY()
+
+public:
+	ASAS_OpponentAIC();
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<USAS_UnitManagerComponent> UnitManagerComp;
+
+	/** Runs the behavior StateTree for this unit. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Components")
+	TObjectPtr<UStateTreeAIComponent> StateTreeComp;
+};
