d635ca5e2a58adf8ad55ff86f3b750c25ef1cbe3
MTCreativeDev <alex.mount@mtcreative.io>
2026-03-06 17:26:59 -0500

    Updated the Gatherer's hut to use a rotating movement component rather than a tick event (Generally we want to avoid units running tick events due do how many actors will be running in a game).
    Created a child of selectable pawn that will handle the logic for checking for inventory events etc.

---
+++ Source/SupplyAndSiege/Private/Core/Pawns/SAS_SP_InventoryCapable.cpp
@@ -0,0 +1,5 @@
+
+
+
+#include "Core/Pawns/SAS_SP_InventoryCapable.h"
+
---
+++ Source/SupplyAndSiege/Public/Core/Pawns/SAS_SP_InventoryCapable.h
@@ -0,0 +1,17 @@
+
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Core/Pawns/SAS_SelectablePawn.h"
+#include "SAS_SP_InventoryCapable.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SUPPLYANDSIEGE_API ASAS_SP_InventoryCapable : public ASAS_SelectablePawn
+{
+	GENERATED_BODY()
+	
+};
