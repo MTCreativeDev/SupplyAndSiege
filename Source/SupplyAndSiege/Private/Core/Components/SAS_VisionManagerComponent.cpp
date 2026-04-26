287c669085ac7ffb727ccbf67a273985a90fe040
ArtyT <24428965+ArtyT@users.noreply.github.com>
2026-04-26 12:30:57 +0200

    feat: implement vision manager register/unregister

--- Source/SupplyAndSiege/Private/Core/Components/SAS_VisionManagerComponent.cpp
+++ Source/SupplyAndSiege/Private/Core/Components/SAS_VisionManagerComponent.cpp
@@ -1,6 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Core/Components/SAS_VisionManagerComponent.h"
+#include "Core/Components/SAS_VisionComponent.h"
 
 USAS_VisionManagerComponent::USAS_VisionManagerComponent()
 {
@@ -19,10 +20,37 @@ void USAS_VisionManagerComponent::EndPlay(const EEndPlayReason::Type EndPlayReas
 
 void USAS_VisionManagerComponent::RegisterComponent(USAS_VisionComponent* Comp)
 {
+	if (!IsValid(Comp))
+	{
+		return;
+	}
+
+	TWeakObjectPtr<USAS_VisionComponent> WeakComp(Comp);
+	AllTargets.AddUnique(WeakComp);
+
+	if (Comp->IsSource())
+	{
+		TArray<TWeakObjectPtr<USAS_VisionComponent>>& TeamSources = Sources.FindOrAdd(Comp->GetCachedTeam());
+		TeamSources.AddUnique(WeakComp);
+	}
 }
 
 void USAS_VisionManagerComponent::UnregisterComponent(USAS_VisionComponent* Comp, ESAS_Team Team)
 {
+	if (Comp == nullptr)
+	{
+		return;
+	}
+
+	TWeakObjectPtr<USAS_VisionComponent> WeakComp(Comp);
+	AllTargets.RemoveSingle(WeakComp);
+
+	if (TArray<TWeakObjectPtr<USAS_VisionComponent>>* TeamSources = Sources.Find(Team))
+	{
+		TeamSources->RemoveSingle(WeakComp);
+	}
+
+	LastVisibleToViewer.Remove(WeakComp);
 }
 
 void USAS_VisionManagerComponent::SetViewingTeam(ESAS_Team NewTeam)
