4bd38d5fcb9d7e206bdccdf7f5b00482b42ef92f
MTCreativeDev <alex.mount@mtcreative.io>
2026-05-11 23:30:30 -0400

    Finished adding in the existing buildings

--- Source/SupplyAndSiege/Private/Core/Actors/SAS_SB_StorageDepot.cpp
+++ Source/SupplyAndSiege/Private/Core/Actors/SAS_SB_StorageDepot.cpp
@@ -2,9 +2,9 @@
 
 
 #include "Core/Actors/SAS_SB_StorageDepot.h"
-#include "Core/Components/SAS_InventoryComponent.h"
+#include "Core/Components/Inventory/SAS_IC_ResourceDepot.h"
 
 ASAS_SB_StorageDepot::ASAS_SB_StorageDepot()
 {
-	Inventory = CreateDefaultSubobject<USAS_InventoryComponent>(TEXT("Inventory"));
+	Inventory = CreateDefaultSubobject<USAS_IC_ResourceDepot>(TEXT("Inventory"));
 }
